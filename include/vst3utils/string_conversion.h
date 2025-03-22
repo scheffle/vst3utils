6609a889750a480753d8cc3e838bdd11452499da
scheffle <scheffle@users.noreply.github.com>
2025-03-22 17:39:34 +0100

    update documentation wording

--- include/vst3utils/string_conversion.h
+++ include/vst3utils/string_conversion.h
@@ -19,7 +19,8 @@ namespace vst3utils {
 //------------------------------------------------------------------------
 /** copy UTF-16 string to an ASCII string
  *
- *  all non ASCII characters are replaced with replacement_char if it is greater zero
+ *  All non-ASCII characters are replaced with the replacement_char if its value is greater than
+ *	zero.
  */
 template<char replacement_char>
 inline std::string copy_utf16_to_ascii (std::u16string_view str)
@@ -37,7 +38,7 @@ inline std::string copy_utf16_to_ascii (std::u16string_view str)
 //------------------------------------------------------------------------
 /** copy UTF-16 string to an ASCII string
  *
- *  all non ASCII characters are removed in this conversion
+ *  All non-ASCII characters are removed during this conversion.
  */
 inline std::string copy_utf16_to_ascii (std::u16string_view str)
 {
@@ -45,7 +46,7 @@ inline std::string copy_utf16_to_ascii (std::u16string_view str)
 }
 
 //------------------------------------------------------------------------
-/** create UTF-16 string from ASCII string */
+/** create an UTF-16 string from an ASCII string */
 inline std::u16string create_utf16_from_ascii (std::string_view ascii)
 {
 	std::u16string result;
@@ -55,7 +56,7 @@ inline std::u16string create_utf16_from_ascii (std::string_view ascii)
 }
 
 //------------------------------------------------------------------------
-/** copy ASCII string to UTF-16 buffer */
+/** copy an ASCII string to an UTF-16 buffer */
 inline void copy_ascii_to_utf16 (std::string_view ascii, char16_t* start, char16_t* end)
 {
 	assert (start < end);
